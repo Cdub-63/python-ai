7fd003889583aaa74d17a336a479455d94ea7659
Christian Wallace <christian.wallace63@gmail.com>
2026-03-30 06:46:24 -0500

    Adding user prompt

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from google import genai
+
+parser = argparse.ArgumentParser(description="A Gemini API Chatbot")
+parser.add_argument("user_prompt", type=str, help="The prompt for the Chatbot to respond to.")
+args = parser.parse_args()
 
 load_dotenv()
 api_key = os.getenv('GEMINI_API_KEY')
@@ -13,7 +18,7 @@ else:
 client = genai.Client(api_key=api_key)
 response = client.models.generate_content(
     model="gemini-2.5-flash",
-    contents = "What's great about Tulsa in 1 paragraph?"
+    contents = args.user_prompt
 )
 
 if response.usage_metadata:
